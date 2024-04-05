d9675199e48ddf0ac35d6be7d8f262cf16721b77
timothy-kumah <timothy.kumah@amalitech.com>
2024-04-05 09:01:58 +0000

    Initial Commit

--- API Filters/Configurations/Filters/MyFilters.cs
+++ API Filters/Configurations/Filters/MyFilters.cs
@@ -1,6 +1,19 @@
-﻿namespace API_Filters.Configurations.Filters
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace API_Filters.Configurations.Filters
 {
-    public class MyFilters
+    public class MyFilters : IActionFilter
     {
+        void IActionFilter.OnActionExecuting(ActionExecutingContext context)
+        {
+            Console.WriteLine("Filter now executing");
+        }
+
+        void IActionFilter.OnActionExecuted(ActionExecutedContext context)
+        {
+            throw new NotImplementedException();
+        }
+
+
     }
 }
